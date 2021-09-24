17ca79bc92dd720914e2abfa33574234bda4474e
Akashkapase9988 <akashkapase9988@gmail.com>
2021-09-24 11:49:15 +0530

    codde for initialize the variable and person

---
+++ Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addressbooksystem
+{
+    class Address
+    { 
+        public string name;
+        public string address;
+        public string phonenumber;
+        public string city;
+        public string state;
+        public string zip; 
+        public string email;
+
+    public Address(string name, string address, string phonenumber, string city, string state, string zip, string email)
+    {
+            this.name = name;
+            this.address = address;
+            this.phonenumber = phonenumber;
+            this.city = city;
+            
+            this.state = state;
+            this.zip = zip;
+            this.email = email;
+    }
+    
+    }
+}
